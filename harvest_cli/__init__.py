cd0424a2788d8146fe860980c93bd22238d4fcb6
Jay Luker <jay_luker@harvard.edu>
2021-07-12 11:33:17 -0400

    MI-198: python 3 compatibility; includes:
    * using pip-compile to generate requirements
    * adding black and flake8 rules and pre-commit hook
    * python 3 updates, almost all done using the `2to3` command
    * arrow objects now use `shift` instead of `replace`
    * a bunch of black formatting changes (wish i'd done this separately)
    * removing some loggly cruft that isn't used anymore

--- harvest_cli/__init__.py
+++ harvest_cli/__init__.py
@@ -1,21 +1,26 @@
-
 import click
 import logging
+
 logging.basicConfig()
 
 click.disable_unicode_literals_warning = True
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 logger = logging.getLogger(__name__)
 
+
 @click.group()
-@click.option('--log-level', default='info',
-              type=click.Choice(['info','debug','warn']))
+@click.option(
+    "--log-level",
+    default="info",
+    type=click.Choice(["info", "debug", "warn"]),
+)
 def cli(log_level):
     logger.setLevel(getattr(logging, log_level.upper()))
     # turn off noisy warnings from elasticsearch/urllib3
-    logging.getLogger('elasticsearch').setLevel(logging.ERROR)
+    logging.getLogger("elasticsearch").setLevel(logging.ERROR)
+
 
 from .zoom import zoom
 from .setup import setup
--- harvest_cli/zoom.py
+++ harvest_cli/zoom.py
@@ -7,12 +7,13 @@ import arrow
 import requests
 from datetime import timedelta, datetime
 from elasticsearch.helpers import bulk as index_bulk
-from geolocation import Geolocate
+from .geolocation import Geolocate
 
 from .utils import es_connection
 from harvest_cli import cli
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 API_BASE_URL = "https://api.zoom.us/v1"
@@ -20,27 +21,47 @@ API_BASE_URL = "https://api.zoom.us/v1"
 
 def yesterday(ctx, param, value):
     if value is None:
-        return (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        return (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
     else:
         return value
 
 
 @cli.command()
-@click.option("--date", callback=yesterday,
-              help="fetch for date, e.g. YYYY-mm-dd; defaults to yesterday.")
-@click.option("--destination", type=click.Choice(['index','stdout']),
-              help="defaults to 'index'", default="index")
-@click.option("--es_host", envvar="ES_HOST", default="localhost:9200",
-              help="Elasticsearch host:port; defaults to $ES_HOST")
-@click.option("--key", envvar="ZOOM_KEY",
-              help="zoom api key; defaults to $ZOOM_KEY")
-@click.option("--secret", envvar="ZOOM_SECRET",
-              help="zoom api secret; defaults to $ZOOM_SECRET")
-@click.option("--geolite", envvar="GEOLITE_PATH",
-              help="filepath to geolite database; defaults to $GEOLITE_PATH")
+@click.option(
+    "--date",
+    callback=yesterday,
+    help="fetch for date, e.g. YYYY-mm-dd; defaults to yesterday.",
+)
+@click.option(
+    "--destination",
+    type=click.Choice(["index", "stdout"]),
+    help="defaults to 'index'",
+    default="index",
+)
+@click.option(
+    "--es_host",
+    envvar="ES_HOST",
+    default="localhost:9200",
+    help="Elasticsearch host:port; defaults to $ES_HOST",
+)
+@click.option(
+    "--key",
+    envvar="ZOOM_KEY",
+    help="zoom api key; defaults to $ZOOM_KEY",
+)
+@click.option(
+    "--secret",
+    envvar="ZOOM_SECRET",
+    help="zoom api secret; defaults to $ZOOM_SECRET",
+)
+@click.option(
+    "--geolite",
+    envvar="GEOLITE_PATH",
+    help="filepath to geolite database; defaults to $GEOLITE_PATH",
+)
 def zoom(date, destination, es_host, key, secret, geolite):
 
-    if destination == 'index':
+    if destination == "index":
         es = es_connection(es_host)
         meetings_index = "meetings-" + date.replace("-", ".")
         sessions_index = "sessions-" + date.replace("-", ".")
@@ -59,20 +80,21 @@ def zoom(date, destination, es_host, key, secret, geolite):
             for s in session_docs:
                 s["geoip"] = g.get(s["ip_address"])
 
-            if destination == 'index':
+            if destination == "index":
                 es.index(
                     index=meetings_index,
                     doc_type="meeting",
                     body=meeting_doc,
-                    id=meeting_doc['uuid']
+                    id=meeting_doc["uuid"],
                 )
                 session_actions = [
                     dict(
                         _index=sessions_index,
-                        _type='session',
-                        _id=s['meeting'] + s['user_id'],
+                        _type="session",
+                        _id=s["meeting"] + s["user_id"],
                         **s
-                    ) for s in session_docs
+                    )
+                    for s in session_docs
                 ]
                 index_bulk(es, session_actions)
             else:
@@ -99,7 +121,9 @@ class ZoomApiException(Exception):
     pass
 
 
-def fetch_records(report_url, params, listkey, countkey='total_records', wait=1):
+def fetch_records(
+    report_url, params, listkey, countkey="total_records", wait=1
+):
     params = params.copy()
     records = []
     logger.debug("get %s" % report_url)
@@ -111,43 +135,46 @@ def fetch_records(report_url, params, listkey, countkey='total_records', wait=1)
         r.raise_for_status()
         response = r.json()
 
-        if 'error' in response.keys():
-            error_code = response['error']['code']
-            error_message = response['error']['message']
+        if "error" in response.keys():
+            error_code = response["error"]["code"]
+            error_message = response["error"]["message"]
             if error_code == 3001:
                 logger.warning(
-                    "Returned error %s: %s, Call: %s, Params: %s" % (error_code, error_message, report_url, params)
+                    "Returned error %s: %s, Call: %s, Params: %s"
+                    % (error_code, error_message, report_url, params)
                 )
                 break
             else:
-                raise ZoomApiException(response['error'])
+                raise ZoomApiException(response["error"])
 
         records.extend([record for record in response[listkey]])
 
-        logger.debug("Fetched %d of %d total records", len(records), response[countkey])
+        logger.debug(
+            "Fetched %d of %d total records", len(records), response[countkey]
+        )
         if len(records) >= response[countkey]:
             break
 
         time.sleep(wait)
 
-        params['page_number'] += 1
+        params["page_number"] += 1
 
     return records
 
 
 def get_active_hosts(date, key, secret):
     params = {
-        'from': date,
-        'to': date,
-        'api_key': key,
-        'api_secret': secret,
-        'page_size': 300,  # max page size
-        'page_number': 1
+        "from": date,
+        "to": date,
+        "api_key": key,
+        "api_secret": secret,
+        "page_size": 300,  # max page size
+        "page_number": 1,
     }
 
-    hosts = fetch_records("/report/getaccountreport", params, 'users')
+    hosts = fetch_records("/report/getaccountreport", params, "users")
 
-    active_host_ids = [host['user_id'] for host in hosts]
+    active_host_ids = [host["user_id"] for host in hosts]
 
     return active_host_ids
 
@@ -156,31 +183,32 @@ def get_series_info(host_ids, key, secret):
     series_info = {}  # series ids mapped to topic and host id
 
     params = {
-        'api_key': key,
-        'api_secret': secret,
-        'page_size': 300,  # max page size
-        'page_number': 1
+        "api_key": key,
+        "api_secret": secret,
+        "page_size": 300,  # max page size
+        "page_number": 1,
     }
 
     for host_id in host_ids:
 
-        params['host_id'] = host_id
+        params["host_id"] = host_id
 
-        series = fetch_records("/meeting/list", params, 'meetings')
+        series = fetch_records("/meeting/list", params, "meetings")
 
         for meeting in series:
-            meeting_id = meeting['id']
+            meeting_id = meeting["id"]
 
             # this shouldn't happen
             if meeting_id in series_info:
                 logger.warning(
                     "Different host_id (%s) returned same meeting (%s)",
-                    host_id, meeting_id
+                    host_id,
+                    meeting_id,
                 )
 
             series_info[meeting_id] = {
-                'host_id': meeting['host_id'],
-                'topic': meeting['topic']
+                "host_id": meeting["host_id"],
+                "topic": meeting["topic"],
             }
 
         time.sleep(1)
@@ -194,26 +222,28 @@ def get_meetings(date, key, secret):
     series_info = get_series_info(host_ids, key, secret)
 
     params = {
-        'from': date,
-        'to': date,
-        'type': 2,  # completed meetings
-        'api_key': key,
-        'api_secret': secret,
-        'page_size': 100,  # max page size
-        'page_number': 1
+        "from": date,
+        "to": date,
+        "type": 2,  # completed meetings
+        "api_key": key,
+        "api_secret": secret,
+        "page_size": 100,  # max page size
+        "page_number": 1,
     }
 
-    meetings = fetch_records("/metrics/meetings", params, 'meetings', wait=60)  # 1 min rate limit
+    meetings = fetch_records(
+        "/metrics/meetings", params, "meetings", wait=60
+    )  # 1 min rate limit
 
     for meeting in meetings:
 
         topic = ""
         host_id = ""
-        series_id = meeting['id']
+        series_id = meeting["id"]
 
         if series_id in series_info:
-            topic = series_info[series_id]['topic']
-            host_id = series_info[series_id]['host_id']
+            topic = series_info[series_id]["topic"]
+            host_id = series_info[series_id]["host_id"]
 
         yield create_meeting_document(meeting, topic, host_id)
 
@@ -223,22 +253,24 @@ def get_sessions_from(date, key, secret):
     url = "/metrics/meetingdetail"
 
     params = {
-        'api_key': key,
-        'api_secret': secret,
-        'type': 2,  # completed meetings
-        'page_size': 100,  # max page size
-        'page_number': 1
+        "api_key": key,
+        "api_secret": secret,
+        "type": 2,  # completed meetings
+        "page_size": 100,  # max page size
+        "page_number": 1,
     }
 
     for meeting_doc in get_meetings(date, key, secret):
 
-        uuid = meeting_doc['uuid']
-        params['meeting_id'] = uuid
-        sessions = fetch_records(url, params, 'participants',
-                                 countkey='participants_count')
+        uuid = meeting_doc["uuid"]
+        params["meeting_id"] = uuid
+        sessions = fetch_records(
+            url, params, "participants", countkey="participants_count"
+        )
 
-        session_docs = [create_sessions_document(session, uuid)
-                        for session in sessions]
+        session_docs = [
+            create_sessions_document(session, uuid) for session in sessions
+        ]
 
         time.sleep(1)
 
@@ -248,25 +280,27 @@ def get_sessions_from(date, key, secret):
 def create_meeting_document(meeting, topic, host_id):
 
     doc = {
-        "uuid": meeting['uuid'],
-        "meeting_series_id": meeting['id'],
+        "uuid": meeting["uuid"],
+        "meeting_series_id": meeting["id"],
         "topic": topic,
         "host": {
             "host_id": host_id,
-            "name": meeting['host'],
-            "email": meeting['email'],
-            "user_type": meeting['user_type']
+            "name": meeting["host"],
+            "email": meeting["email"],
+            "user_type": meeting["user_type"],
         },
-        "start_time": meeting['start_time'],
-        "end_time": meeting['end_time'],
-        "duration": to_seconds(meeting['duration']),
-        "participant_sessions": meeting['participants'],  # not unique participants
-        "has_pstn": meeting['has_pstn'],
-        "has_voip": meeting['has_voip'],
-        "has_3rd_party_audio": meeting['has_3rd_party_audio'],
-        "has_video": meeting['has_video'],
-        "has_screen_share": meeting['has_screen_share'],
-        "recording": meeting['recording']
+        "start_time": meeting["start_time"],
+        "end_time": meeting["end_time"],
+        "duration": to_seconds(meeting["duration"]),
+        "participant_sessions": meeting[
+            "participants"
+        ],  # not unique participants
+        "has_pstn": meeting["has_pstn"],
+        "has_voip": meeting["has_voip"],
+        "has_3rd_party_audio": meeting["has_3rd_party_audio"],
+        "has_video": meeting["has_video"],
+        "has_screen_share": meeting["has_screen_share"],
+        "recording": meeting["recording"],
     }
 
     return doc
@@ -275,33 +309,32 @@ def create_meeting_document(meeting, topic, host_id):
 def create_sessions_document(session, meeting_uuid):
 
     try:
-        j = arrow.get(session['join_time'])
-        l = arrow.get(session['leave_time'])
-        duration = (l - j).seconds
+        join_time = arrow.get(session["join_time"])
+        leave_time = arrow.get(session["leave_time"])
+        duration = (leave_time - join_time).seconds
     except Exception as e:
         logger.warning(
-            "Failed duration calc for session '%s': %s",
-            session['id'], str(e)
+            "Failed duration calc for session '%s': %s", session["id"], str(e)
         )
         duration = None
 
     doc = {
         "meeting": meeting_uuid,
-        "id": session['id'],
-        "user_id": session['user_id'],
-        "user_name": session['user_name'],
-        "device": session['device'],
-        "ip_address": session['ip_address'],
-        "country": session['cn'],
-        "city": session['city'],
-        "network_type": session['network_type'],
-        "join_time": session['join_time'],
-        "leave_time": session['leave_time'],
+        "id": session["id"],
+        "user_id": session["user_id"],
+        "user_name": session["user_name"],
+        "device": session["device"],
+        "ip_address": session["ip_address"],
+        "country": session["cn"],
+        "city": session["city"],
+        "network_type": session["network_type"],
+        "join_time": session["join_time"],
+        "leave_time": session["leave_time"],
         "duration": duration,
-        "share_application": session['share_application'],  # bool
-        "share_desktop": session['share_desktop'],  # bool
-        "share_whiteboard": session['share_whiteboard'],  # bool
-        "recording": session['recording']  # bool
+        "share_application": session["share_application"],  # bool
+        "share_desktop": session["share_desktop"],  # bool
+        "share_whiteboard": session["share_whiteboard"],  # bool
+        "recording": session["recording"],  # bool
     }
 
     return doc
@@ -319,10 +352,12 @@ def to_seconds(duration):
         return int(delta.total_seconds())
     except ValueError:
         import pytimeparse
-        logger.warning("Duration parsing failed on input '%s'; falling back to pytimeparse. ", duration)
+
+        logger.warning(
+            "Duration parsing failed on input '%s'; falling back to pytimeparse. ",
+            duration,
+        )
         duration = pytimeparse.parse(duration)
         if duration is None:
             logger.warning("Even the mighty pytimeparse failed!")
         return duration
-
-
